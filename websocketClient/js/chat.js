2754c910bce8592477003e6f9b37af10592b7346
hencsat46 <name66437@gmail.com>
2023-11-18 12:08:11 +0300

    get messages front

--- websocketClient/js/chat.js
+++ websocketClient/js/chat.js
@@ -4,17 +4,41 @@ const ws = new WebSocket("ws://localhost:3000/ws")
 console.log(ws.readyState)
 ws.onopen = function() {
     console.log("Connected")
+    getMessages()
+
+
+}
+
+async function getMessages() {
+    const request = new Request("http://localhost:3000/getmessages", {
+        method: "GET",
+        mode: "cors",
+    })
+
+    const response = await (await fetch(request)).json()
+    
+    if (response.Payload[0].Message_text != "Internal Server Error") {
+        for (let i = 0; i < response.Payload.length; ++i) {
+            drawMessage(response.Payload[i].Message_text)
+        }
+    }
+}
+
+function drawMessage(text) {
+    const message = document.createElement("div")
+    message.classList.add("message")
+    message.innerText = text
+    document.querySelector(".messages").append(message)
 }
 
 ws.onmessage = (event) => {
     const responseMessage = JSON.parse(event.data)
     
     console.log(responseMessage)
+
+    drawMessage(responseMessage.message)
     
-    const message = document.createElement("div")
-    message.classList.add("message")
-    message.innerText = responseMessage.message
-    document.querySelector(".messages").append(message)
+    
 }
 
 function sendMessage() {
